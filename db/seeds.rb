2e739c79d3ecef4595303c0bc948d48d96622bed
Dennis Neely <dennis.neely22@gmail.com>
2020-02-12 12:43:52 +0100

    reviews and restaurants controllers added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating database with seeds..."
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian norwegian american].sample
+  )
+end
+
+puts "Database updated!"
